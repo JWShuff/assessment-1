a3c9a8b51d7169e129924872fa60162407bba0ed
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-04 13:07:34 -0400

    Expanded null case tests and code.

--- optimal_change.py
+++ optimal_change.py
@@ -1,3 +1,12 @@
 def optimal_change(item_cost, amount_paid):
-    result_string = ""
+
+    no_change_due = "No change due."
+    change_due = amount_paid - item_cost    
+    result_string = f"The optimal change for an item that costs {item_cost} with an amount paid of {amount_paid} is "
+    # Catch if no change due before further handling.
+    if change_due == 0:
+        return no_change_due
+    elif change_due < 0:
+        return "You have underpaid."
+    print(result_string)
     return result_string
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -8,14 +8,29 @@ class TestOptimalChange(unittest.TestCase):
     """
     When you call optimal_change you get a string back:
     """
-
     def test_returns_a_string(self):
 
         self.assertTrue(type(optimal_change(1, 1)) == type("string"))
+    
     """
-    When you call optimal_change with no change required 
+    When you call optimal_change with no change required you get back "No change due."
     """
+    def test_returns_no_change(self):
+        self.assertTrue(optimal_change(1, 1) == "No change due.")
 
+    """
+    When you call optimal change with cost > paid you get back "You have underpaid."
+    """
+    def test_returns_underpaid(self):
+        self.assertTrue(optimal_change(4,1) == "You have underpaid.")
+
+    """
+    When you call optimal change, it properly returns the correct amount of change.
+    """
+    def test_returns_proper_change(self):
+        self.assertTrue(optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
+        self.assertTrue(optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
 
 if __name__ == '__main__':
     unittest.main()
