038cfce46e80c70d95c39077c8e78b834e91aa3c
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-04 14:52:19 -0400

    Revised logic to catch penny/pennies at final step (pennies will always be last)

--- optimal_change.py
+++ optimal_change.py
@@ -1,37 +1,23 @@
-from decimal import *
-getcontext().prec = 4
+from unittest import result
+
 
 def optimal_change(item_cost:float, amount_paid:float):
-    change_due = Decimal(amount_paid) - Decimal(item_cost) 
-    print(change_due)
-    result_string = f"The optimal change for an item that costs {item_cost} with an amount paid of {amount_paid} is "
+    change_due = int(round(amount_paid * 100)) - int(round(item_cost*100))
+    result_string = f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is "
 #The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
 
     value_to_currency_dict = {
-        '$100 bill': Decimal(100.00), 
-        '$50 bill' : Decimal(50.00), 
-        '$20 bill' : Decimal(20.00), 
-        '$10 bill' : Decimal(10.00),
-        '$5 bill' : Decimal(5.00),
-        '$1 bill' : Decimal(1.00),
-        'quarter' : Decimal(.25),
-        'dime' : Decimal(.10),
-        'nickel' : Decimal(.05),
-        'penny' : Decimal(.01)
+        '$100 bill': 10000,
+        '$50 bill' : 5000,
+        '$20 bill' : 2000,
+        '$10 bill' : 1000,
+        '$5 bill' : 500,
+        '$1 bill' : 100,
+        'quarter' : 25,
+        'dime' : 10,
+        'nickel' : 5,
+        'penny' : 1,
     }
-    
-    # [
-    #     (100, "$100 bill"),
-    #     (50, "$50 bill"),
-    #     (20, "$20 bill"),
-    #     (10, "$10 bill"),
-    #     (5, "$5 bill"),
-    #     (1, "$1 bill"),
-    #     (.25, "quarter"),
-    #     (.10, "dime"),
-    #     (.05, "nickel"),
-    #     (.01, "penny")
-    # ]
 
     # Catch if no change due or underpaid before further handling.
     if change_due == 0:
@@ -41,22 +27,27 @@ def optimal_change(item_cost:float, amount_paid:float):
     else:
         for key in value_to_currency_dict:
             a = change_due // value_to_currency_dict[key]
-            print(value_to_currency_dict[key])
-            print ("a is {a}")
-            if a > 1 and value_to_currency_dict[key] != .01:
+            # print(key)
+            # print (f"a is {a}")
+
+            #exit condition
+            if change_due % value_to_currency_dict[key] == 0:
+                if a > 1 and key != "penny":
+                    result_string += f"and {a} {key}s."
+                elif a == 1 and key != "penny":
+                    result_string += f"and 1 {key}."
+                elif a == 1 and key == "penny":
+                    result_string += "and 1 {key}."
+                elif a > 1 and key == "penny":
+                    result_string += f"and {a} pennies."
+            elif  a > 1:
+
                 result_string += f"{int(a)} {key}s, "
 
-            elif a > 1 and value_to_currency_dict[key] == .01:
-                pennies_str = "pennies"
-                result_string += f"{int(a)} {pennies_str}"
-            elif a == 1 and value_to_currency_dict[key] == .01:
-                result_string += f"and {a} {key}"
             elif a == 1:
                 result_string += f"1 {key}, "
+            
             change_due = change_due % value_to_currency_dict[key]
-            print(result_string)
-            print(change_due)
+
     return result_string
 
-
-optimal_change(62.13, 100)
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -27,10 +27,12 @@ class TestOptimalChange(unittest.TestCase):
     """
     When you call optimal change, it properly returns the correct amount of change.
     """
-    def test_returns_proper_change(self):
-        self.assertTrue(optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+    def test_returns_proper_change1(self):
 
-        self.assertTrue(optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+        self.assertEqual(optimal_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
+    def test_returns_proper_change2(self):
+        self.assertEqual(optimal_change(31.51, 50), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
 
 if __name__ == '__main__':
     unittest.main()
