637553910b848c594b1a05bdea1ebddf24f996ec
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-04 14:11:11 -0400

    Using Decimal is partially working.

--- optimal_change.py
+++ optimal_change.py
@@ -1,20 +1,23 @@
-def optimal_change(item_cost:float, amount_paid:float):
+from decimal import *
+getcontext().prec = 4
 
-    change_due = amount_paid - item_cost    
+def optimal_change(item_cost:float, amount_paid:float):
+    change_due = Decimal(amount_paid) - Decimal(item_cost) 
+    print(change_due)
     result_string = f"The optimal change for an item that costs {item_cost} with an amount paid of {amount_paid} is "
 #The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
 
     value_to_currency_dict = {
-        '$100 bill': 100.00, 
-        '$50 bill' : 50.00, 
-        '$20 bill' : 20.00, 
-        '$10 bill' : 10.00,
-        '$5 bill' : 5.00,
-        '$1 bill' : 1.00,
-        'quarter' : .25,
-        'dime' : .10,
-        'nickel' : .05,
-        'penny' : .01
+        '$100 bill': Decimal(100.00), 
+        '$50 bill' : Decimal(50.00), 
+        '$20 bill' : Decimal(20.00), 
+        '$10 bill' : Decimal(10.00),
+        '$5 bill' : Decimal(5.00),
+        '$1 bill' : Decimal(1.00),
+        'quarter' : Decimal(.25),
+        'dime' : Decimal(.10),
+        'nickel' : Decimal(.05),
+        'penny' : Decimal(.01)
     }
     
     # [
@@ -38,12 +41,21 @@ def optimal_change(item_cost:float, amount_paid:float):
     else:
         for key in value_to_currency_dict:
             a = change_due // value_to_currency_dict[key]
-
+            print(value_to_currency_dict[key])
+            print ("a is {a}")
             if a > 1 and value_to_currency_dict[key] != .01:
                 result_string += f"{int(a)} {key}s, "
-            
+
+            elif a > 1 and value_to_currency_dict[key] == .01:
+                pennies_str = "pennies"
+                result_string += f"{int(a)} {pennies_str}"
+            elif a == 1 and value_to_currency_dict[key] == .01:
+                result_string += f"and {a} {key}"
+            elif a == 1:
+                result_string += f"1 {key}, "
             change_due = change_due % value_to_currency_dict[key]
-    print(result_string)
+            print(result_string)
+            print(change_due)
     return result_string
 
 
