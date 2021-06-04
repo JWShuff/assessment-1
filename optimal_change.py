8427add4904461f7b1cbadb51380ebda7fc264ac
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-04 16:28:20 -0400

    More tests and adjustments to code

--- optimal_change.py
+++ optimal_change.py
@@ -19,46 +19,61 @@ def optimal_change(item_cost, amount_paid):
         'nickel' : 5,
         'penny' : 1,
     }
+    # This change_bool flips when our return string is appended to, 
+    # used to determine proper string formation when no further change is due
+    change_bool = False
 
     # Catch if no change due or underpaid before further handling.
     if change_due == 0:
         return "No change due."
-    elif change_due < 0:
+    elif change_due < 1:
         return "You have underpaid."
-    else:
-        counter = 0
-        for key in value_to_currency_dict:
-            a = change_due // value_to_currency_dict[key]
+    # Need this to catch issues introduced by conversion above to integer values
+    elif (amount_paid - item_cost) < .009:
+        return "You aren't paying with legal tender."
 
-            #exit condition
+    # Begin main logic for string generation
+    else:
+        # From top of currency dict to bottom, evaluate change due against value of a key
+        for key in value_to_currency_dict:
+            # Tracks the # of bills of a given currency due by taking floor of change divided by value of key 
+            a = change_due // value_to_currency_dict[key]
+            # EXIT CONDITION
+            # (Pennies included, because if first string append op is the final, 
+            # it will be pennies or other precise amount of change), and return result_string
             if change_due % value_to_currency_dict[key] == 0:
-                if counter == 0:
+                if change_bool == False:
                     if a > 1:
                         if key != "penny":
                             result_string += f"{a} {key}s."
+                            return result_string
                         else:
                             result_string += f"{a} pennies."
+                            return result_string
                     else:
                         result_string += f"{a} {key}."
+                        return result_string
+                #Appends final string of units to result_string, and returns.
                 else:
                     if a > 1 and key != "penny":
                         result_string += f"and {a} {key}s."
+                        return result_string
                     elif a == 1 and key != "penny":
                         result_string += f"and 1 {key}."
+                        return result_string
                     elif a == 1 and key == "penny":
                         result_string += f"and 1 {key}."
+                        return result_string
                     elif a > 1 and key == "penny":
                         result_string += f"and {a} pennies."
-
+                        return result_string
+            # Base logic for appending {a} # of $bill/coins that aren't pennies.
             elif  a > 1:
                 result_string += f"{a} {key}s, "
-                counter += 1
+                change_bool = True
             elif a == 1:
                 result_string += f"1 {key}, "
-                counter += 1
-
+                change_bool = True
+            # Modulo of change_due gives remaining value for which to make change.
             change_due = change_due % value_to_currency_dict[key]
-            
-
-    return result_string
-
+            # Back to top of the For Key loop to begin again.
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,15 +1,12 @@
 import unittest
-
 from optimal_change import optimal_change
-
 
 class TestOptimalChange(unittest.TestCase):
 
     """
-    When you call optimal_change you get a string back:
+    When you call optimal_change you get a string back.
     """
     def test_returns_a_string(self):
-
         self.assertTrue(type(optimal_change(1, 1)) == type("string"))
     
     """
@@ -28,7 +25,6 @@ class TestOptimalChange(unittest.TestCase):
     When you call optimal change, it properly returns the correct amount of change.
     """
     def test_returns_proper_change1(self):
-
         self.assertEqual(optimal_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
 
     def test_returns_proper_change2(self):
@@ -36,7 +32,7 @@ class TestOptimalChange(unittest.TestCase):
 
 
     """
-    When you call optimal change with change due of .01, you get back 1 penny
+    When you call optimal change with change due of .01, you get back 1 penny.
     """
     def test_handles_one_penny(self):
         self.assertEqual(optimal_change(.02, .03), "The optimal change for an item that costs $0.02 with an amount paid of $0.03 is 1 penny.")
@@ -59,8 +55,15 @@ class TestOptimalChange(unittest.TestCase):
         self.assertEqual(optimal_change(1956.43, 3156.75), "The optimal change for an item that costs $1956.43 with an amount paid of $3156.75 is 12 $100 bills, 1 quarter, 1 nickel, and 2 pennies.")
 
     """
-
+    Test returns correct full dollar amount of change.
     """
+    def test_returns_four_dollars(self):
+        self.assertEqual(optimal_change(6,10), "The optimal change for an item that costs $6 with an amount paid of $10 is 4 $1 bills.")
+    """
+    Test detects rounding issues at edge case conditions (.6 pennies more than cost.
+    """
+    def test_int_conversion_and_rounding(self):
+        self.assertEqual(optimal_change(.1, .106), "You aren't paying with legal tender.")
 
 if __name__ == '__main__':
     unittest.main()
